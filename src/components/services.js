f97d8b59dc48fe616568c468d359d022c264eba3
saimsohailKH <saimsohil466@gmail.com>
2024-08-05 21:09:08 +0500

    show list of services in landing page

--- src/components/services.js
+++ src/components/services.js
@@ -1,3 +1,5 @@
+import { services } from "../data/services";
+
 function Services() {
   return (
     <section id="services" class="services section light-background">
@@ -7,72 +9,21 @@ function Services() {
       </div>
 
       <div class="container">
-        <div class="row gy-4">
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-            <div class="service-item  position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-mountain-city"></i>
+        <div class="row d-flex justify-content-center gy-4">
+          {
+            services.map((service, index) => (
+              <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100" key={index}>
+                <div class="service-item  position-relative">
+                  <div class="icon">
+                    <i class={service.icon}></i>
+                  </div>
+                  <h3>{service.title}</h3>
+                  <p>{service.description}</p>
+                  <a href={`/service/${service.id}`} class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
+                </div>
               </div>
-              <h3>Hospital Loans</h3>
-              <p>Empower your hospital with tailored loan options.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-arrow-up-from-ground-water"></i>
-              </div>
-              <h3>Contractor Loans</h3>
-              <p>Affordable loans for contractor growth and success.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-compass-drafting"></i>
-              </div>
-              <h3>Unsecured Business Loans</h3>
-              <p>Secure funds without collateral for your business</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-trowel-bricks"></i>
-              </div>
-              <h3>Supplier Fianance</h3>
-              <p>Optimize cash flow with supplier finance solutions.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-helmet-safety"></i>
-              </div>
-              <h3>Home Loans</h3>
-              <p>Flexible home loans tailored to you.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="fa-solid fa-arrow-up-from-ground-water"></i>
-              </div>
-              <h3>Loan Against Special Properties</h3>
-              <p>Finance your needs with special property loans.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
+            ))
+          }
         </div>
       </div>
     </section>
--- src/data/services.js
+++ src/data/services.js
@@ -2,78 +2,78 @@ export const services = [
   {
     id: 1,
     title: 'Home Loans',
-    description: '',
-    icon: '',
+    description: 'We understand that buying your first home is one the biggest financial decisions you will make...',
+    icon: 'fa-solid fa-house',
     kind: 'Individual'
   },
   {
     id: 2,
     title: 'Personal Loans',
-    description: '',
-    icon: '',
+    description: 'Whatever stage of life we might be in, sometimes we need funds in a quick turnaround time...',
+    icon: 'fa-solid fa-person',
     kind: 'Individual'
   },
   {
     id: 3,
     title: 'Car Loan',
-    description: '',
-    icon: '',
+    description: 'It’s good to get a pre-approval before you go to the dealer or commit someone to purchase their vehicle At VED...',
+    icon: 'fa-solid fa-car',
     kind: 'Individual'
   },
   {
     id: 4,
     title: 'Refinance / Debt Consolidation',
-    description: '',
-    icon: '',
+    description: 'Do it again for your own good. Doing a health check on your current home loan and comparing it with what’s in the market can save you thousands...',
+    icon: 'fa-solid fa-dollar-sign',
     kind: 'Individual'
   },
   {
     id: 5,
     title: 'Commercial Loans',
-    description: '',
-    icon: '',
+    description: 'Are you planning to start a new business, buy a franchise or expand a current business, we can help you...',
+    icon: 'fa-solid fa-dumpster-fire',
     kind: 'Corporates'
   },
   {
     id: 6,
     title: 'Cash Flow Lending',
-    description: '',
-    icon: '',
+    description: 'Cash flow is an issue that keeps a lot of business owners awake at night...',
+    icon: 'fa-solid fa-microchip',
     kind: 'Corporates'
   },
   {
     id: 7,
     title: 'Asset / Equipment Finance',
-    description: '',
-    icon: '',
+    description: 'Equipment Finance enables you to obtain financing for business equipment, machinery and vehicles (trucks and tractors)...',
+    icon: 'fa-solid fa-dollar-sign',
     kind: 'Corporates'
   },
   {
     id: 8,
     title: 'Line of credit / Business Overdraft',
-    description: '',
-    icon: '',
+    description: 'Lines of credit are approved “loan” facilities that you can draw down on at any time that suits your business, without any further approvals...',
+    icon: 'fa-solid fa-business-time',
     kind: 'Corporates'
   },
   {
     id: 9,
     title: 'LO Doc Loan',
-    description: '',
-    icon: '',
+    description: 'Low Documentation (Low Doc) loans provide flexible financing solutions for self-employed people...',
+    icon: 'fa-solid fa-paperclip',
     kind: 'Corporates'
   },
   {
     id: 10,
     title: 'Debt Removal',
-    description: '',
-    icon: '',
+    description: 'If you have bad credit, you know how hard life can become! From everyday purchases to investments in your future, your credit history can come back to haunt you...',
+    icon: 'fa-solid fa-dollar-sign',
     kind: ''
   },
   {
     id: 11,
     title: 'Utility Connection Service',
-    description: '',
-    icon: '',
+    description: 'We understand that buying your first home is one the biggest financial decisions you will make...',
+    icon: 'fa-brands fa-connectdevelop',
     kind: ''
   }
 ];
