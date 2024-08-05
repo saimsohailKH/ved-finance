20b6251806533231e8418b1ecc5b5d8ca8182e8c
saimsohailKH <saimsohil466@gmail.com>
2024-08-05 21:17:22 +0500

    fix bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -34,6 +34,15 @@ function NavBar(props) {
         }
       });
     });
+
+    document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+      navmenu.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.parentNode.classList.toggle('active');
+        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+        e.stopImmediatePropagation();
+      });
+    });
   }, [])
 
   const activePage = (path) => {
--- src/main.js
+++ src/main.js
@@ -39,14 +39,6 @@
   /**
    * Toggle mobile nav dropdowns
    */
-  // document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-  //   navmenu.addEventListener('click', function(e) {
-  //     e.preventDefault();
-  //     this.parentNode.classList.toggle('active');
-  //     this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-  //     e.stopImmediatePropagation();
-  //   });
-  // });
 
   /**
    * Preloader
